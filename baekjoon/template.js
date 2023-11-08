09749af8be1ab16f639560dbe78d78c956d548c0
jmlee9707 <jmlee9707@naver.com>
2023-11-08 16:59:14 +0900

    :memo: Add text

--- baekjoon/template.js
+++ baekjoon/template.js
@@ -1,5 +1,6 @@
+// 🌱 풀이 날짜
+// 🔥 백준 (문제 번호 ) : (문제 이름)
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-console.log(input);
