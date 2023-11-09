13f894ffa3d799d8905a5e9ea60f1b91b5da7c04
jmlee9707 <jmlee9707@naver.com>
2023-11-09 16:26:57 +0900

    :sparkles: 231109 : 새싹 solved

---
+++ baekjoon/basic/25083.js
@@ -0,0 +1,10 @@
+// 🌱 풀이 날짜 : 2023. 11. 09 (목)
+
+// 🔥 백준 25083 : 새싹
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
